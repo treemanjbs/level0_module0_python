5d6e6edf60bbc649c1d0277158b95c06a0e02258
league <treemanjbs>
2024-06-27 12:01:24 -0700

    6/27/24

--- _02_code_flow/_a_turtle_spiral.py
+++ _02_code_flow/_a_turtle_spiral.py
@@ -14,23 +14,24 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # Make a new turtle
-    turtle= turtle.Turtle()
+    bruh= turtle.Turtle()
     # This code sets our shape to a turtle
-    turtle.shape('turtle')
+    bruh.shape('circle')
     # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
-    turtle.speed('10')
+    bruh.speed(10)
     # Set your turtle's color using .color('green')
-    turtle.color('')
+    turtle.color('PURPLE')
     # Use a loop to repeat a the code below 50 times
     for i in range(50):
         # Set the turtle color to a random color
-        turtle.randomcolor
+        bruh.color(get_random_color())
         # Move the turtle (5*i) pixels. 'i' is the loop variable
-        turtle
+        bruh.forward(5*i)
+
         # Turn the turtle (360/7) degrees to the right
-         
+        bruh.right(360/7)
         # Change the turtle width to 'i' (the loop variable)
-        
+        bruh.width(i)
         # Check the pattern against the picture in the recipe. If it matches, you are done!
     
 # ===================== DO NOT EDIT THE CODE BELOW ============================
